21dde0efc934b41d6bec8a08a76a3856ac37c9d0
Angie Lu <88385045+doupeach@users.noreply.github.com>
2022-05-25 00:54:17 +0800

    Change favicon and button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
-import './Navbar.css';
-import logo from '../images/Popcorn_logo.png'
-import searchLogo from '../images/search_small.png'
-import memberLogo from '../images/user_circle.png'
-
+import "./Navbar.css";
+import logo from "../images/Popcorn_logo.png";
+import searchLogo from "../images/search_small.png";
+import memberLogo from "../images/user_circle.png";
 
 function Navbar() {
   return (
     <div className="navbar">
-      <img id="logo" src={logo} />
+      <div id="logo-wrap">
+        <img id="logo" src={logo} />
+      </div>
       <div id="navbar-link">
         <div>Home</div>
         <div>New & Popular</div>

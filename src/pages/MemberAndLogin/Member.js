724d26a7b19427484afc02838e1eef485c185ecb
Angie Lu <88385045+doupeach@users.noreply.github.com>
2022-06-17 02:32:51 +0800

    Fix issues

--- src/pages/MemberAndLogin/Member.js
+++ src/pages/MemberAndLogin/Member.js
@@ -129,19 +129,19 @@ function Member({ uid }) {
             </HeaderH2>
 
             <CardBtnDiv justifyContent={"center"}>
-              {/* <GlowTitle>
+              {<GlowTitle>
                 <MemberPageButton
                   backgroundColor={"transparent"}
                   padding={"0px"}
                 >
-                  <Link to="/myfavs">
+                  <Link to="/myfav">
                     <img src={favLogo} style={{ width: "35px" }} />
                   </Link>
                 </MemberPageButton>
                 <HeaderH2 margin={"2% 20px 1% 5px;"} color={"#cacaca"}>
                   Favorites
                 </HeaderH2>
-              </GlowTitle> */}
+              </GlowTitle>}
 
               <Link to="/mylist">
                 <GlowTitle filter={"drop-shadow(0 0 5px rgba(245, 130, 0, 1))"}>
